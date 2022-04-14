6c727330baf22d5c55bc99845ce6230597da8b92
themion7 <themion@naver.com>
2022-04-14 20:34:54 +0900

    인증서 위치 변경

--- cert/createCert.sh
+++ cert/createCert.sh
@@ -2,10 +2,12 @@ dirpath=`dirname $0`
 cd $dirpath
 dirpath=`pwd`
 
-keyName=private
-pemName=public
-certificateName=myNote
+keyName=private.pem
+pemName=public.pem
+certName=myNote
 
-openssl req -x509 -sha256 -nodes -newkey rsa:2048 -keyout $keyName.key -out $pemName.pem
-openssl pkcs12 -export -inkey $keyName.key -in $pemName.pem -name $certificateName -out $certificateName.p12 -caname root
-cp $certificateName.p12 $dirpath/../backend/src/main/resources/
+openssl req -x509 -sha256 -nodes -newkey rsa:2048 -keyout $keyName -out $pemName
+openssl pkcs12 -export -inkey $keyName -in $pemName -name $certName -out $certName.p12 -caname root
+# cp $certName.p12 $dirpath/../backend/src/main/resources/
+# cp $keyName $dirpath/../frontend/.cert/
+# cp $pemName $dirpath/../frontend/.cert/
