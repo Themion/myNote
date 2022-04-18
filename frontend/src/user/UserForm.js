4dc616a4e059baac0877c5831b4a6e49c452ba0f
themion7 <themion@naver.com>
2022-04-18 10:07:31 +0900

    login에 fallback 추가

--- frontend/src/user/UserForm.js
+++ frontend/src/user/UserForm.js
@@ -12,29 +12,43 @@ const passwordOnChange = (password) => {
 
 export const SignUp = () => {
     const callback = (res) => {
-        window.location.href = '/login'
+        window.location.href = '/login?signup'
+    }
+
+    const fallback = (data) => {
+        const inputs = document.querySelectorAll(".card-body .form-control")
+        const logs = {}
+
+        inputs.forEach(input => logs[input.name] = [])
+
+        data.errors.forEach(err => {
+            logs[err.field].push(err.defaultMessage)
+        })
+
+        inputs.forEach(input => validate(input, logs[input.name])) 
     }
 
     return (
         <UserForm 
             name="Sign up" 
             inputs={[{
-                    for: "username"
-                }, {
-                    for: "password",
-                    type: "password",
-                    onChange: passwordOnChange
-                }, {
-                    for: "password_check",
-                    type: "password",
-                    onChange: passwordOnChange
-                }, {
-                    for: "nickname",
-                    required: false
+                for: "username"
+            }, {
+                for: "password",
+                type: "password",
+                onChange: passwordOnChange
+            }, {
+                for: "password_check",
+                type: "password",
+                onChange: passwordOnChange
+            }, {
+                for: "nickname",
+                required: false
             }]} 
             link={{ to: "/login", text: "Log in" }} 
             url="/user" 
-            callback={callback} />
+            callback={callback}
+            fallback={fallback} />
     )
 }
 
@@ -46,20 +60,25 @@ export const Login = () => {
         window.location.href = '/'
     }
 
+    const fallback = (data) => {
+        window.location.href = '/login?error'
+    }
+
     return (
         <UserForm 
             name="Log in" 
             inputs={[{
-                    for: "username"
-                }, {
-                    for: "password",
-                    type: "password",
-                    onChange: passwordOnChange
+                for: "username"
+            }, {
+                for: "password",
+                type: "password",
+                onChange: passwordOnChange
             }]} 
             link={{ to: "/signup", text: "Sign up" }} 
             url="/login" 
             method="POST"
-            callback={callback} />
+            callback={callback}
+            fallback={fallback} />
     )
 }
 
@@ -72,7 +91,9 @@ export const UserForm = (props) => {
     const onSubmit = (e) => {
         e.preventDefault();
 
-        const inputs = document.querySelectorAll(".card-body .form-control"), data = {};
+        const inputs = document.querySelectorAll("form .form-control")
+        const data = {};
+        
         let is_valid = true
 
         // document.querySelector("form.needs-validation").classList.add("was-validated")
@@ -83,39 +104,29 @@ export const UserForm = (props) => {
             is_valid = is_valid && !input.classList.contains("is-invalid")
         })
 
-        const fallback = (data) => {
-            const logs = {}
-            inputs.forEach(input => logs[input.name] = [])
-
-            data.errors.forEach(err => {
-                logs[err.field].push(err.defaultMessage)
-            })
-
-            inputs.forEach(input => validate(input, logs[input.name])) 
-        }
-
         if (is_valid)
-            send(props.url, props.method, data, props.callback, fallback)
+            send(props.url, props.method, data, props.callback, props.fallback)
     }
 
     const input_list = []
 
     // Input을 만들기 위한 값을 모두 넣은 뒤
-    props.inputs.forEach(input => {
-        input_list.push(<Input 
+    props.inputs.forEach(input => input_list.push(
+        <Input 
             key={input.for} 
             for={input.for} 
             type={input.type}
             required={input.required} 
             onChange={input.onChange}
             classList={input.classList}
-            value={input.value} />)
-    })
+            value={input.value} />
+    ))
 
     // UserForm을 Return
     return (
         <div className={`card ` + styles.card}>
             <div className="card-body">
+                {props.alert}
                 <a className="float-end btn btn-outline-primary" href={props.link.to}>{props.link.text}</a>
                 <h4 className="card-title mb-4 mt-1">{props.name}</h4>
                 <form onSubmit={onSubmit} className="needs-validation" noValidate>
