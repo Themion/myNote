d4cd8b01e51414268d28e557037013cd05ebc7b2
themion7 <themion@naver.com>
2022-04-18 10:58:11 +0900

    UserForm의 link 버튼 개선

--- frontend/src/user/UserConfig.js
+++ frontend/src/user/UserConfig.js
@@ -1,15 +1,79 @@
-import { localStorageAuth } from "../utils/utils"
+import { localStorageAuth, getNickname } from "../utils/utils"
+
+import { UserForm } from "./UserForm"
+
+const passwordOnChange = (password) => {
+    if (password !== document.querySelector('input[name=password]').value) 
+        return "비밀번호와 비밀번호 확인이 같지 않습니다."
+    return ""
+}
+
+const ChangePassword = () => {
+    const callback = () => {
+        window.location.href = '/user'
+    }
+
+    const fallback = () => {
+        window.location.href = '/user?passwordChangeError'
+    }
+
+    return (
+        <UserForm 
+            name="Change Password" 
+            inputs={[{
+                for: "current password"
+            }, {
+                for: "password",
+                type: "password",
+                minLength: 6,
+                maxLength: 30
+            }, {
+                for: "password_check",
+                type: "password",
+                onChange: passwordOnChange
+            }]} 
+            url="/user" 
+            callback={callback}
+            fallback={fallback} />
+    )
+}
+
+const ChangeNickname = () => {
+    const callback = () => {
+        window.location.href = '/user'
+    }
+
+    const fallback = () => {
+        window.location.href = '/user?nicknameChangeError'
+    }
+
+    return (
+        <UserForm 
+            name="Change Nickname" 
+            inputs={[{
+                for: "password"
+            }, {
+                for: "nickname"
+            }]} 
+            url="/user" 
+            callback={callback}
+            fallback={fallback} />
+    )
+}
 
 export const Config = () => {
     if (window.localStorage.getItem(localStorageAuth) === null) {
         window.location.href = "/login"
     }
 
-    console.log(window.localStorage.getItem(localStorageAuth))
-
     return (
         <div>
-            {/* 사용자 비밀번호 / 닉네임 수정 및 사용자 탈퇴 */}
+            <h2>{getNickname()}</h2>
+            <hr />
+            <div>
+                <ChangePassword />
+                <ChangeNickname />
+            </div>
         </div>
     )
 }
