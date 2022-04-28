1f10fa2d9010180999c80cf844ecf3d24c5effb9
themion7 <themion@naver.com>
2022-04-28 18:37:51 +0900

    세션 관리 기능을 별개 파일로 분리

---
+++ frontend/src/utils/session.js
@@ -0,0 +1,52 @@
+import axios from "axios"
+import { baseURL, redirect } from "./utils"
+
+export const Auth = "authorization"
+export const accessTokenStorage = "access-token"
+export const refreshTokenStorage = "refresh-token"
+
+export const getAccessToken = () => window.localStorage.getItem(accessTokenStorage)
+export const setAccessToken = (accessToken) => window.localStorage.setItem(accessTokenStorage, accessToken)
+export const removeAccessToken = () => window.localStorage.removeItem(accessTokenStorage)
+
+export const getRefreshToken = () => window.localStorage.getItem(refreshTokenStorage)
+export const setRefreshToken = (refreshToken) => window.localStorage.setItem(refreshTokenStorage, refreshToken)
+export const removeRefreshToken = () => window.localStorage.removeItem(refreshTokenStorage)
+
+export const getTokenPayload = (token) => {
+    return JSON.parse(atob(token.split(".")[1]))
+}
+
+export const getNickname = () => {
+    const accessToken = getAccessToken()
+    return accessToken ? getTokenPayload(accessToken).nickname : "User"
+}
+
+export const isTokenExpired = (token) => {
+    return ( getTokenPayload(token).exp * 1000) <= new Date().getTime()
+}
+
+export const requestAccessToken = () => {
+    const headers = {}
+    headers[Auth] = "Bearer " + getRefreshToken()
+
+    const config = {
+        url: '/session',
+        method: 'GET',
+        headers: headers,
+        data: {},
+        baseURL: baseURL
+    }
+
+    axios(config).then(res => setAccessToken(res.data[accessTokenStorage]))
+}
+
+export const manageTokens = () => {
+    const accessToken = getAccessToken()
+    const refreshToken = getRefreshToken()
+
+    if (refreshToken && isTokenExpired(refreshToken)) {
+        removeRefreshToken()
+        redirect('/login')
+    } else if (accessToken && isTokenExpired(accessToken)) requestAccessToken()
+}
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,17 +1,14 @@
 import axios from "axios"
+import { Auth, getAccessToken } from "./session"
 
-const baseURL = "https://localhost:8443"
-const localStorageAuth = "authorization"
+export const baseURL = "https://localhost:8443"
 
-export const getSession = () => window.localStorage.getItem(localStorageAuth)
-export const setSession = (session) => window.localStorage.setItem(localStorageAuth, session)
-export const removeSession = () => window.localStorage.removeItem(localStorageAuth)
 export const redirect = (path) => window.location.href = path
 
 export const send = (url, method, data, callback, fallback) => {
     const headers = {}
 
-    headers[localStorageAuth] = getSession()
+    headers[Auth] = "Bearer " + getAccessToken()
 
     const config = {
         url: url,
@@ -25,28 +22,4 @@ export const send = (url, method, data, callback, fallback) => {
         console.log(err.response)
         fallback(err.response)
     })
-}
-
-export const getNickname = () => {
-    const JWT = getSession()
-    if (JWT === null) return "User"
-    
-    const payload = JSON.parse(atob(JWT.split(".")[1]))
-    
-    return payload.nickname
-}
-
-export const getExpiration = () => {
-    const JWT = getSession()
-    if (JWT === null) return new Date().getTime() + 10
-
-    const payload = JSON.parse(atob(JWT.split(".")[1]))
-
-    return payload.exp * 1000
-}
-
-export const sessionTimeOut = () => {
-    if (getExpiration() <= new Date().getTime()) {
-        removeSession()
-    }
 }
